b243d66894ce5329ed46b27486a9a058ab489290
Artyom <arkrasovski1996@gmail.com>
2021-08-19 20:29:05 +0300

    feat: add burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,32 @@
 document.querySelector(".navbar-btn").addEventListener("click", () => {
   document.querySelector(".navbar-wrapper").classList.toggle("change");
 });
+
+const burgerBtn = document.querySelector(" header > span");
+const navWrapper = document.querySelector(".adaptate_nav_wrapper");
+const nav = document.querySelector(".adaptate_nav_wrapper .adaptate_nav");
+const links = document.querySelectorAll(".adaptate_nav > ul.links li a");
+const wrapper = document.querySelector(".wrapper");
+console.log(links);
+burgerBtn.addEventListener("click", () => {
+  navWrapper.style.width = "100%";
+  navWrapper.style.height = wrapper.offsetHeight + "px";
+  nav.style.width = "30%";
+  document.body.overflow = "hidden";
+});
+
+navWrapper.addEventListener("click", (e) => {
+  if (e.target === navWrapper) {
+    navWrapper.style.width = "0%";
+    nav.style.width = "0%";
+    navWrapper.style.height = 0;
+  }
+});
+
+links.forEach((item) => {
+  item.addEventListener("click", () => {
+    navWrapper.style.width = "0%";
+    nav.style.width = "0%";
+    navWrapper.style.height = 0;
+  });
+});
